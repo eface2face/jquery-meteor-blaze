aa96c247cb87cff016dcce6c3e827580199da2e7
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2015-02-07 23:28:35 +0100

    updated meteor object map

--- dist/jquery-meteor-blaze.devel.js
+++ dist/jquery-meteor-blaze.devel.js
@@ -10284,26 +10284,18 @@ module.exports = function(Meteor) {
 	var Tracker = Meteor.Tracker;
 	var ReactiveObjectMap;
 
-	/**
-	 * @class
-	 * @instanceName reactiveVar
-	 * @summary Constructor for a ReactiveObjectMap, which represents a reactive map of objects.
-	 * Creates an object composed of keys generated from the results of running each element of collection through iteratee. The corresponding value of each key is the last element responsible for generating the key. The iteratee function is bound to thisArg and invoked with three arguments; (value, index|key, collection).
-
-	collection (Array|Object|string): The collection to iterate over.
-	[iteratee=_.identity] (Function|Object|string): The function invoked per iteration. If a property name or object is provided it is used to create a ".property" or ".matches" style callback respectively.
-	[thisArg] (*): The this binding of iteratee.
-	 * @locus Client
-	 * @param {(Array|Object|string} collection The initial value to to iterate over
-	 * @param {Function|Object|string} [iteratee=_.identity] Optional. The function invoked per iteration. If a property name or object is provided it is used to create a ".property" or ".matches" style callback respectively.
-	 */
-	ReactiveObjectMap = function(collection, iteratee) {
+	ReactiveObjectMap = function() {
 		if (!(this instanceof ReactiveObjectMap))
 		// called without `new`
 			return new ReactiveObjectMap(collection, iteratee);
 
-		this.map = _.indexBy(collection, iteratee);
+		this.map = {};
 		this.dep = new Tracker.Dependency;
+	};
+
+	ReactiveObjectMap.prototype.assign = function(collection, iteratee) {
+		this.map = _.indexBy(collection, iteratee);
+		this.dep.changed();
 	};
 
 	ReactiveObjectMap.prototype.get = function(key) {
